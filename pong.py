6b79c0163680d2a26d25eacbda83d1307567439e
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-02 21:43:05 +0545

    py

--- pong.py
+++ pong.py
@@ -7,6 +7,12 @@ win.setup(width=800, height=600)
 win.tracer(0)
 
 # Bat A
+batA = turtle.Turtle()
+batA.speed(0)
+batA.shape("square")
+batA.color("white")
+batA.penup()
+batA.goto(-350, 0)
 
 # Bat B
 
