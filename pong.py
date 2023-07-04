db4a8a43f9760b80bbe3c8c01463799aa400089f
ChhitizV <chhitizvaidya10@gmail.com>
2023-07-04 21:18:04 +0545

    points

--- pong.py
+++ pong.py
@@ -36,6 +36,11 @@ ball.dy = -0.07
 
 pen = turtle.Turtle()
 pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0", align="center", font = ("Roboto", 24, "bold"))
 
 
 
